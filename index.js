a36732de6ec2a3ae7ecfd55f57faad0403100947
shiv-anya <sg90883@gmail.com>
2022-07-28 10:49:26 +0530

    added functionality on times

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const burger = document.getElementsByClassName("burger");
 const menu = document.getElementsByClassName("menu");
+const times = document.getElementsByClassName("times");
 // burger.style.color = "red";
 burger[0].addEventListener("click", (e) => {
-  if (menu[0].style.display === "block") {
-    menu[0].style.display = "none";
-  } else {
-    menu[0].style.display = "block";
-  }
+  menu[0].style.display = "block";
+  times[0].style.display = "block";
+  burger[0].style.display = "none";
+});
+times[0].addEventListener("click", (e) => {
+  times[0].style.display = "none";
+  burger[0].style.display = "block";
+  menu[0].style.display = "none";
 });
 // console.log(burger[0].a);
 console.log("hello");
